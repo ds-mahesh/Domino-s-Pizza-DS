c1ffa8aa80a93bb0c9164e698f601bae9e002d1e
ds-mahesh <mahesh.chand@dotsquare.com>
2023-02-06 19:23:02 +0530

    monday

---
+++ src/config/answersHeadlessConfig.ts
@@ -0,0 +1,20 @@
+
+export const AnswerExperienceConfig = {
+  limit:3,
+  locale: "en",
+  apiKey : "e859ea0ef72f3b53c8330106e2434683",
+  verticalKey : "locations",
+  experienceKey : "dominos-pizza",
+  experienceVersion: "STAGING",
+  locationRadius: 804672,
+  sessionTrackingEnabled: true,
+  endpoints: {
+    universalSearch: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/query",
+    verticalSearch: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/vertical/query",
+    questionSubmission: "https://liveapi-sandbox.yext.com/v2/accounts/me/createQuestion",
+    universalAutocomplete: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/autocomplete",
+    verticalAutocomplete: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/vertical/autocomplete",
+    filterSearch: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/filtersearch",
+
+  }
+}
